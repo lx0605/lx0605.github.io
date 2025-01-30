5b1e7b5cea420bfb8bac58205160b24cf0e9df49
lx0605 <luoxiaoustc@gmail.com>
2025-01-29 22:03:44 -0800

    updated bio

--- bio.js
+++ bio.js
@@ -4,9 +4,8 @@
 
 const bioContent = `
 <p> Hello, I am a computational earth scientist in Pacific Northwest National Laboratory (PNNL). My research focuses on the reactive transport of radioactive noble gas in geologic media. 
-    I led the development of high-resolution geologic model and high-performance computing workflow to simulate evolution of noble gas in the vadose zone. 
-    I further built simulation pipeline to solve inversion problems for soil properties. If you are interested in geologic models, stay tuned! </p>
-
+    I led the development of high-resolution geologic model and high-performance computing workflow to simulate the evolution of background radioactivity in the vadose zone. 
+    I further built simulation pipeline to tackle inverse problems to predict soil properties. If you are interested in geologic models, stay tuned! </p>
 <p>When I'm not coding, you can find me [your interests or hobbies]. I believe in [your professional philosophy or values], and I'm always looking for new challenges and opportunities to grow in my field.</p>
 <p>Feel free to check out my projects below or connect with me on <a href="https://github.com/lx0605" target="_blank">GitHub</a> and <a href="https://www.linkedin.com/in/luoxiaoustc" target="_blank">LinkedIn</a>.</p>
 `;
