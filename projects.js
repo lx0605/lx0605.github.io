cd95935316061533ef57735cc6072a1c18d0a6b8
lx0605 <luoxiaoustc@gmail.com>
2025-02-22 23:22:15 -0800

    revised the project descriptions

--- projects.js
+++ projects.js
@@ -1,19 +1,39 @@
 // projects.js
 
 const projectsContent = [
-    {
-        name: "Simulating Seismic Wavelet Propagation with PyTorch",
-        description: "This article explores the use of PyTorch tensors to simulate seismic wavelet propagation" +
-                    " and solve the inverse problem for calculating material properties using a neural network-inspired approach" +
-                    " $ \\frac{\\partial^2 u(x, t)}{\\partial t^2} = c^2 \\nabla^2 u(x, t)$",
-        technologies: "Python, PyTorch",
+        {
+        name: "3D Gridded Subsurface Data Interpolation with Inverse Distance Weighting",
+        description: `
+                    Subsurface phenomena are difficult to observe and subsurface properties (e.g., contaminant concentrations, hydraulic conductivity) often vary spatially but are measured only at discrete points (e.g., well bores),
+                    leaving unknowns or "missing data" at critical spots. These unknowns complicate predictions (e.g., contaminant plume migration) and simulations (e.g., transport modeling with incomplete initial concentration), rendering problems ill-posed.
+                    This work addresses one of the challenge of modeling 3D gridded subsurface data from discrete data points by applying an anisotropic inverse distance weighting (IDW) method.
+
+                    The IDW approach interpolates properties between sparse data points by assigning weights inversely proportional to the distance (raised to a power) from the target location. To account for directional variability,
+                    the method incorporates anisotropic scaling factors (ax=ay=6, az=1), reflecting stronger spatial correlation along the horizontal directions than the vertical direction.
+                    Using well-bore concentration data (left figure), the IDW method generates a 3D grid model (right figure) to estimate properties between wells, enabling accurate plume predictions.
+                    In addition, the hexagonal grid mesh geometry, along with the concentration data, can be exported to PFLOTRAN input formats, facilitating seamless integration with reactive transport modeling work.
+                        
+                    `,
+        technologies: "Python, Scipy, NumPy, PyVista, Paraview",
         //technologies: "To be build",
-        link: "https://github.com/yourusername/project1",
+        //link: "https://github.com/yourusername/project1",
         images: ["images/model_mesh_1.png", "images/model_mesh_2.png"]
     },
+    // {
+    //     name: "Simulating Seismic Wavelet Propagation with PyTorch",
+    //     description: "This article explores the use of PyTorch tensors to simulate seismic wavelet propagation" +
+    //                 " and solve the inverse problem for calculating material properties using a neural network-inspired approach" +
+    //                 " $ \\frac{\\partial^2 u(x, t)}{\\partial t^2} = c^2 \\nabla^2 u(x, t)$",
+        
+    //                 technologies: "Python, PyTorch",
+    //     //technologies: "To be build",
+    //     link: "https://github.com/yourusername/project1",
+    //     images: ["images/model_mesh_1.png", "images/model_mesh_2.png"]
+    // },
     {
         name: "Partial Correlation Calculator",
-        description: "A tool to calculate the partial correlation matrix based on user input.",
+        description: `When analyzing simulation outputs, it is common to compute covariance to identify which parameters most influence the objective function across multiple runs. 
+                     A tool to calculate the partial correlation matrix based on user input can be quite helpful.`,
         technologies: "JavaScript, HTML, CSS",
         link: "partial-correlation.html"
     }
